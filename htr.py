5cc5b67d609fa4ea5f1e6801b31761f54cfbb165
Devansh Pratap Singh <53232548+devanshpratapsingh@users.noreply.github.com>
2020-12-26 23:33:16 +0530

    Update htr.py

--- htr.py
+++ htr.py
@@ -13,3 +13,15 @@ mnist=tf.keras.datasets.mnist
 x_train = tf.keras.utils.normalize(x_train, axis=1)
 x_test = tf.keras.utils.normalize(x_test, axis=1)
 
+#defining the model, which'll have a input layer, two hidden layers and an output layer
+model = tf.keras.models.Sequential()
+model.add(tf.keras.layers.Flatten(input_shape=(28,28)))  #flatten means it's a simple feet forwaed neural network
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))  #dense means all the neurons are connected to
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))#previous and the next layer
+model.add(tf.keras.layers.Dense(units=128, activation=tf.nn.relu))
+model.add(tf.keras.layers.Dense(units=10, activation=tf.nn.softmax))
+
+model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+
+
+
